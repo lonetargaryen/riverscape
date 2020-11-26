1b4c38df46fc4d40a3f4bc003d36816b731cd063
lonetargaryen5 <swarnim.sonawane@gmail.com>
2020-11-27 00:29:18 +0530

    Added onClick to launch the camera.

--- app/src/main/java/com/example/riverscape/MainActivity.java
+++ app/src/main/java/com/example/riverscape/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.riverscape;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the cameraButton */
+    public void cameraButtonListener(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } catch (ActivityNotFoundException e) {
+            // display error state to the user
+            Context context = getApplicationContext();
+            CharSequence text = "Could not open camera.";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
     }
 }
